c7d0dbf9120b00e26ac9ada2fc27c9b0bd93f55c
Abstract Programmer <AbstractProgrammer@example.com>
2024-02-15 21:58:14 +0530

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentDetails.Controllers
+{
+    public class HomeController : Controller
+    {
+        StudentDetailsEntity _context = new StudentDetailsEntity();
+        public ActionResult Index()
+        {
+            var listofdata = _context.StudentDetails.ToList();
+            return View(listofdata);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+public ActionResult Create(StudentDetail model)
+{
+_context.StudentDetails.Add(model);
+_context.SaveChanges();
+ViewBag.Message = "Data insert succefully";
+return RedirectToAction("Index");
+}
+[HttpGet]
+public ActionResult Edit(int id)
+{
+var data = _context.StudentDetails.Where(x => x.StudentId == id).FirstOrDefault();
+return View(data);
+}
+[HttpPost]
+public ActionResult Edit(StudentDetail Model)
+{
+    var data = _context.StudentDetails.Where(x => x.StudentId == Model.StudentId).FirstOrDefault();
+    if (data != null)
+    {
+    data.StudentCity = Model.StudentCity;
+    data.StudentName = Model.StudentName;
+    data.StudentFeez = Model.StudentFeez;
+    _context.SaveChanges();
+}
+return RedirectToAction("Index");
+        }
+        public ActionResult Detail(int id)
+{
+    var data = _context.StudentDetails.Where(x => x.StudentId == id).FirstOrDefault();
+    return View(data);
+}
+public ActionResult Delete(int id)
+{
+    var data = _context.StudentDetails.Where(x => x.StudentId == id).FirstOrDefault();
+    _context.StudentDetails.Remove(data);
+    _context.SaveChanges();
+    ViewBag.Message = "record deleted successfully";
+    return RedirectToAction("Index");
+}
+    }
+}
---
+++ Controllers/StudentDetailsController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StudentDetails;
+
+
+namespace StudentDetails.Controllers
+{
+    public class StudentDetailsController : Controller
+    {
+        private StudentDetailsEntity db = new StudentDetailsEntity();
+
+        // GET: StudentDetails
+        public ActionResult Index()
+        {
+            return View(db.StudentDetails.ToList());
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(StudentDetail data)
+        {
+            db.StudentDetails.Add(data);
+            db.SaveChanges();
+            ViewBag.Message = "data inserted successfully";
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var data = db.StudentDetails.Where(x => x.StudentId == id).FirstOrDefault();
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Edit(StudentDetail model)
+        {
+            var data = db.StudentDetails.Where(x => x.StudentId ==model.StudentId).FirstOrDefault();
+            if (data != null)
+            {
+                data.StudentCity =model. StudentCity;
+                data.StudentName = model.StudentName;
+                data.StudentFeez = model.StudentFeez;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult Detail(int id)
+        {
+            var data = db.StudentDetails.Where(x => x.StudentId == id).FirstOrDefault();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+           var data= db.StudentDetails.Where(x=>x.StudentId==id).FirstOrDefault();
+            db.StudentDetails.Remove(data);
+            db.SaveChanges();
+            ViewBag.Message = "data deleted successfully";
+            return RedirectToAction("Index");
+        }
+
+    }
+}
